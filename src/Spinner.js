599fd87f9abcfd0b1b1ca738151740866f7cf461
Elise Muellerleile <eliamue@live.com>
2021-06-10 15:21:09 -0500

    js files added

---
+++ src/Spinner.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Spinner extends Component {
+    render() {
+        return (
+            <div>
+                <img src="https://64.media.tumblr.com/c99a579db3ae0fc164bf4cca148885d3/tumblr_mjgv8kEuMg1s87n79o1_400.gif" alt="pikachu running"></img>
+            {/* <img src="https://i.pinimg.com/originals/e4/ae/17/e4ae172d255a14f58be19fdb45a74447.gif" alt="pika"></img> */}
+            </div>
+        )
+    }
+}
