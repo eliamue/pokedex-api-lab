455d2a6795ef0a1b35f7188dfc9727bfcf2585f7
Elise Muellerleile <eliamue@live.com>
2021-06-14 13:23:19 -0500

    nav links added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import Header from './Header.js';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import ListPage from './ListPage.js';
 import Footer from './Footer.js';
+import Home from './Home.js';
 import './App.css';
+import PokeDetail from './PokemonDetail.js'
 
 function App() {
   return (
-    <div className="full">
-      <Header />
-      <ListPage />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="full">
+        <Header />
+        <Switch>
+          <Route path="/pokemon/:id" exact component={PokeDetail}/>
+          <Route path="/pokemon" exact component={ListPage}/>
+          <Route path="/" exact component={Home}/>
+        </Switch>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import './Header.css'
 
 export default class Header extends Component {
     render() {
         return (
             <header>
                 <h1>Look At All These Chickens</h1>
+                <div className="links">
+                    <NavLink exact to="/">Home</NavLink>
+                    <NavLink exact to="/pokemon">Pokedex</NavLink>
+                </div>
             </header>
         )
     }
