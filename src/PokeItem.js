b0787126b217c79208f6d10bc9d448e8abeb071e
Elise Muellerleile <eliamue@live.com>
2021-06-14 13:00:52 -0500

    new pages added

--- src/PokeItem.js
+++ src/PokeItem.js
@@ -8,17 +8,30 @@ export default class Pokeitem extends Component {
                 <img className="pokeimg" src={this.props.data.url_image} alt={this.props.data.pokemon} />
                 <section className="stats">
                     <h3 className="type">Type:</h3> 
-                        {this.props.data.type_1}, {this.props.data.type_2}
+                        <p>
+                            {this.props.data.type_1}, 
+                            {this.props.data.type_2}
+                        </p>
                     <h4 className="attack">Attack:</h4>
-                        {this.props.data.attack}
+                        <p>
+                            {this.props.data.attack}
+                        </p>
                     <h4 className="defense">Defense:</h4>
-                        {this.props.data.defense}
+                        <p>
+                            {this.props.data.defense}
+                        </p>
                     <h4 className="hp">HP:</h4> 
-                        {this.props.data.hp}
+                        <p>
+                            {this.props.data.hp}
+                        </p>
                     <h4 className="abilities">Special Abilities:</h4> 
-                        {this.props.data.ability_1}, 
-                        {this.props.data.ability_2}, 
-                        {this.props.data.ability_hidden}
+                        <p>
+                            {this.props.data.ability_1}
+                            <br></br>  
+                            {this.props.data.ability_2}
+                            <br></br>
+                            {this.props.data.ability_hidden}
+                        </p>
                 </section>
             </div>
         )
